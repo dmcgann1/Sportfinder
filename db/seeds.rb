09f9b387ce7008101ea55ad5759c21744d375ccf
dmcgann1 <dmcgann1@gmail.com>
2014-07-13 23:11:47 -0400

    added some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
+User.delete_all
 Facility.delete_all
+Like.delete_all
+Review.delete_all
+Vote.delete_all
 
-f1 = Facility.create!(name: 'club 1', sport_type: 'tennis', area: 'Boston')
-f2 = Facility.create!(name: 'club 2', sport_type: 'tennis', area: 'Boston')
-f3 = Facility.create!(name: 'club 3', sport_type: 'tennis', area: 'Boston')
-f4 = Facility.create!(name: 'club 4', sport_type: 'golf', area: 'Boston')
-f5 = Facility.create!(name: 'club 5', sport_type: 'golf', area: 'New York')
+diarmuid = User.new
+diarmuid.email = 'dmcgann1@gmail.com'
+diarmuid.encrypted_password = '$2a$10$y2uSEcU4UNuwa.Jvr1bkyOrMnykpWYxDnIdAfmaqbLrRkgaDoyk1S'
+diarmuid.save!
+
+tom = User.new
+tom.email = 'tom@example.com'
+tom.encrypted_password = '$2a$10$LYSdJ1u2o9hJnzxRQVCf/OxMeVlC6UVDEwp7s9jHqS79DEb11Dp5q'
+tom.save!
+
+bob = User.new
+bob.email = 'bryan@example.com'
+bob.encrypted_password = '$2a$10$i80tKbiVkuVXRGZ9CapcDeI6Pgj8yTV7HUeuDUhuQ5yyPB3qGc5mS'
+bob.save!
+
+bryan = User.new
+bryan.email = 'bob@example.com'
+bryan.encrypted_password = '$2a$10$.ctKri4RuKCkES.63VLr3uL8jfQvVP6iktjI0CpGuG5rENebWVufu'
+bryan.save!
+
+patsy = User.new
+patsy.email = 'patsy@example.com'
+patsy.encrypted_password = '$2a$10$ZF5sKJxwhVfdYamn7FgPN..RIMNqL5de3pdJ6KXfJfVBN0cYVtriy'
+patsy.save!
+
+
+
+
+
+
+
